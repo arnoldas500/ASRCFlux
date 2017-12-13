e3362be9ed65b0a2a480d0825a92abd8bac540b8
arnoldas kurbanovas <akurbanovas@albany.edu>
2017-12-13 17:43:30 -0500

    connecting to postgres db and populating table

--- importFluxDB.py
+++ importFluxDB.py
@@ -1,15 +1,59 @@
 import sqlalchemy as sql
 import pandas as pd
 
-pg = sql.create_engine('postgresql://:5433/flux')
+'''
+*********************
+start list
+['2017', '11', '01']
+end list: 
+['2017', '12', '13']
+*********************
+'''
 
-df = pd.read_csv('/flux/2017/12/13/20171213_FLUX_BKLN_Flux_NYSMesonet.csv')
+def populate(dateStartList, dateEndList):
 
-df.columns = df.columns.str.lower()
+    #make database connection
+    pg = sql.create_engine('postgresql://:5433/flux')
+    
+    #getting range of dates
+    def perdelta(start, end, delta):
+        curr = start
+        while curr < end:
+            yield curr
+            curr += delta
+    #ex date(2017, 10, 10)
+    dfList = []
+    #getting all of the selected csv files according to date and sotring them into a list
 
-try:
-    df.drop(columns=['table', 'record_number', 'year', 'month', 'day', 'hour', 'minute', 'second', 'microsecond', 'timestamp_start', 'timestamp_end'], inplace=True)
-except Exception as e:
-    pass
+    x=0
+    last = datetime.date(int(dateEndList[0]), int(dateEndList[1]), int(dateEndList[2]))
+    lastPlus = last + timedelta(days=1)
+    for curDate in perdelta(datetime.date(int(dateStartList[0]),int(dateStartList[1]),int(dateStartList[2])), lastPlus, timedelta(days=1)):
+        try:
+            
+            curDateSTR = curDate.strftime('%Y/%m/%d')
+        
+            dateSlash = curDateSTR.replace("-","/")
+            dateStrp = curDateSTR.replace("/","")
+            df74 = pd.read_csv('/flux/' + dateSlash + '/' + dateStrp + '_FLUX_'+ select +'_Flux_NYSMesonet.csv')
+            fileExists = True
+            
+        except FileNotFoundError as e:
+            print(e)
+        x+=1
 
-df.to_sql('nysmesonet', pg, if_exists='append',index=False)
+    if(not fileExists):
+        raise FileNotFoundError("no data found at all!!!")
+
+    
+
+    df = pd.read_csv('/flux/2017/12/13/20171213_FLUX_BKLN_Flux_NYSMesonet.csv')
+
+    df.columns = df.columns.str.lower()
+
+    try:
+        df.drop(columns=['table', 'record_number', 'year', 'month', 'day', 'hour', 'minute', 'second', 'microsecond', 'timestamp_start', 'timestamp_end'], inplace=True)
+    except Exception as e:
+        pass
+
+    df.to_sql('nysmesonet', pg, if_exists='append',index=False)
