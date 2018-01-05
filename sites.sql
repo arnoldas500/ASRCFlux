3dd2aa4a54034ad4da78b2c0dabab071105d2972
arnoldas kurbanovas <akurbanovas@albany.edu>
2018-01-05 15:23:58 -0500

    Create sites.sql

---
+++ sites.sql
@@ -0,0 +1,2 @@
+create table sites (stid varchar primary key, number smallint, name varchar, latitude float, longitude float, elevation float, county varchar, nearest_city varchar, state varchar, distance_from_town float, direction_from_town varchar, climate_division smallint, climate_division_name varchar, wfo varchar, commissioned timestamp, decommissioned timestamp);
+
